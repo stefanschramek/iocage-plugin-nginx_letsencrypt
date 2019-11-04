94c6b7213853023ce47bbdfc3e05663ba4020b9e
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-11-04 19:28:36 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,10 @@
 rehash
 
 sysrc -f /etc/rc.conf nginx_enable="YES"
+sysrc -f /etc/rc.conf firewall_enable="YES"
+sysrc -f /etc/rc.conf firewall_type="workstation"
+sysrc -f /etc/rc.conf firewall_myservices="80/tcp 443/tcp"
+sysrc -f /etc/rc.conf firewall_allowservices="any"
 
+sudo service ipfw start
 sudo service nginx start
